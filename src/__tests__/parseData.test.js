e655760db4158833c788dffaff98134b28029565
Chazza <github@whalecoiner.com>
2021-04-15 18:18:04 +0200

    Add full fixtures

--- src/__tests__/parseData.test.js
+++ src/__tests__/parseData.test.js
@@ -3,6 +3,9 @@ const path = require('path')
 const parseData = require('../parseData')
 
 let instagramProfileHtml = fs.readFileSync(path.join(__dirname, 'fixtures/instagramUserProfile.html'))
+let instagramVideoHtml = fs.readFileSync(path.join(__dirname, 'fixtures/instagramVideo.html'))
+let instagramPostHtml = fs.readFileSync(path.join(__dirname, 'fixtures/instagramPost.html'))
+let instagramPhotoHtml = fs.readFileSync(path.join(__dirname, 'fixtures/instagramPhoto.html'))
 
 describe('Checking the basics', () => {
 
@@ -10,8 +13,26 @@ describe('Checking the basics', () => {
     await expect(parseData()).rejects.toThrow('No data supplied.')
   })
 
-  test('Passes with lovely data', async () => {
+  test('Fails with invalid HTML', async () => {
+    await expect(parseData('<p>LOOK AT MY HTML</p>')).rejects.toThrow('Could not find Instagram code in HTML')
+  })
+
+  test('Passes with valid Profile HTML', async () => {
     await expect(parseData(instagramProfileHtml.toString())).resolves.toHaveProperty('entry_data')
   })
+
+  test('Passes with valid video HTML', async () => {
+    await expect(parseData(instagramVideoHtml.toString())).resolves.toHaveProperty('entry_data')
+  })
+
+  test('Passes with valid photo HTML', async () => {
+    await expect(parseData(instagramPhotoHtml.toString())).resolves.toHaveProperty('entry_data')
+  })
+
+  test('Passes with valid Post/Carousel/sidecar HTML', async () => {
+    await expect(parseData(instagramPostHtml.toString())).resolves.toHaveProperty('entry_data')
+  })
+
+
 
 })
--- src/parseData.js
+++ src/parseData.js
@@ -3,7 +3,10 @@ const dataRegEx = /window\._sharedData\s?=\s?({.+);<\/script>/;
 const parseData = async (html) => {
   try {
     if (!html) throw new Error('No data supplied.')
-    let dataString = html.match(dataRegEx)[1]
+    let initialMatchAttempt = html.match(dataRegEx)
+    if (initialMatchAttempt === null) throw new Error('Could not find Instagram code in HTML')
+
+    let dataString = initialMatchAttempt[1]
     let json = JSON.parse(dataString)
     return json
   } catch (error) {
